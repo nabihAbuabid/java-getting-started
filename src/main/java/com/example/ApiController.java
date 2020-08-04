f531c1df60d0bfbf8599a8be5b20d14c39259cf5
Nabih Abu Abid <nabuabid@salesforce.com>
2020-08-04 15:11:27 +0300

    add sender

--- src/main/java/com/example/ApiController.java
+++ src/main/java/com/example/ApiController.java
@@ -1,12 +1,20 @@
 package com.example;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+
 @Controller
+@Resource
 public class ApiController {
+    private static final Logger LOG = LoggerFactory.getLogger(ApiController.class);
+
     @Autowired
     Sender sender;
 
@@ -18,12 +26,19 @@ public class ApiController {
     @RequestMapping("/hello")
     @ResponseBody
     String home() {
-        Foo foo = new Foo();
-        String user = "User" + System.currentTimeMillis();
-        foo.setName(user);
-        foo.setDescription("Hello " + user);
-        sender.send(foo);
-        return "Hello " + user;
+        try {
+            Foo foo = new Foo();
+            String user = "User" + System.currentTimeMillis();
+            LOG.info("Say helo to " + user);
+            foo.setName(user);
+            foo.setDescription("Hello " + user);
+            sender.send(foo);
+            return "Hello " + user;
+        } catch (Exception e) {
+            String stackTrace = ExceptionUtils.getStackTrace(e);
+            LOG.info("ERRRRRRRRRRRRRRRROR in hello api " + stackTrace);
+            return stackTrace;
+        }
     }
 
 }
